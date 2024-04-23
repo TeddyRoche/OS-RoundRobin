527cb8adc8abb2fbdfd3dd800a1f52582df055d3
Theodore Roche <teddyroche2000@gmail.com>
2024-04-23 09:53:13 -0400

    1

--- OS RoundRobin/OS RoundRobin.cpp
+++ OS RoundRobin/OS RoundRobin.cpp
@@ -3,62 +3,14 @@
 #include <mutex>
 #include <thread>
 #include <chrono>
-#define Time_Quantum 50
 
 using namespace std;
 
-struct Process
-{
-    string name;
-    int arrival_time;
-    int burst_time;
-    int remaining_time;
-    int priority; // New attribute for priority
 
-    // Comparison function for priority scheduling
-    bool operator<(const Process& other) const
-    {
-        return priority < other.priority;
-    }
-};
-
-template<typename T> 
-class Queue {
-
-    void push()
-    {
-
-    }
-    void pop()
-    {
-
-    }
-    bool empty()
-    {
-
-    }
-
-};
-
-void checkArrivingProcesse()
-{
-
-}
-
-void executeProcess()
-{
-
-}
-
-void threadFunction()
-{
-
-}
 
 int main()
 {
 	cout << "Hello World!\n";
-    //code to add queue :)
     
 }
 
